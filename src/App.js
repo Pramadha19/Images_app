8d87c39dae35f00d1322c1226640e89e2e48488b
Pramadha Yaramakala <pramadhayaramakala@Pramadhas-MacBook-Pro.local>
2024-03-21 23:10:37 +0530

    image

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      if (!searchQuery) return; // Don't fetch if search query is empty
+      
+      const accessKey = "AiVqwSLLPbggtp7wpbF2YoVPmrPzTymOj0iHgiR35EU"; // Replace "YOUR_ACCESS_KEY" with your actual Unsplash access key
+      const url = `https://api.unsplash.com/search/photos?page=1&query=${searchQuery}`;
+
+      try {
+        const response = await fetch(url, {
+          headers: {
+            Authorization: `Client-ID ${accessKey}`
+          }
+        });
+        const data = await response.json();
+        setImages(data.results);
+      } catch (error) {
+        console.error("Error fetching images:", error);
+      }
+    };
+
+    fetchImages();
+  }, [searchQuery]);
+
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Image Generator app</h1>
+        <form>
+          <input type='search' placeholder='Enter Text....' value={searchQuery} onChange={handleSearchChange}></input>
+        </form>
+      </div>
+      <div className="image-container">
+        {images.map((image) => (
+          <img key={image.id} src={image.urls.regular} alt={image.alt_description} />
+        ))}
+      </div>
     </div>
   );
 }
